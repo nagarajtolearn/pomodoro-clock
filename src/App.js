917939de59424006e8578d0f886647f12ed9a1e6
nagarajtolearn <nagarajtolearn@gmail.com>
2022-07-28 00:31:35 +0530

    updated style and features

--- src/App.js
+++ src/App.js
@@ -1,56 +1,135 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
+  const [play, setPlay] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(1500);
+  const [timingType, setTimingType] = useState("SESSION");
+
+  const timeout = setTimeout(function () {
+    if (timeLeft && play) setTimeLeft(timeLeft - 1);
+  }, 1000);
+
+  const clock = () => {
+    if (play) {
+      timeout;
+      resetTimer();
+    } else {
+      clearTimeout(timeout);
+    }
+  };
+  useEffect(() => {
+    clock();
+  }, [play, timeLeft, timeout]);
+
+  const handlePlay = () => {
+    clearTimeout(timeout);
+    setPlay(!play);
+  };
+
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if (!timeLeft && timingType === "SESSION") {
+      setTimeLeft(breakLength * 60);
+      setTimingType("BREAK");
+      audio.play();
+    }
+    if (!timeLeft && timingType === "BREAK") {
+      setTimeLeft(sessionLength * 60);
+      setTimingType("SESSION");
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  };
+
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    setTimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingType("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  const timeFormater = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const title = timingType === "SESSION" ? "Session" : "Break";
 
   const handleBreakIncrease = () => {
-    if (breakLength < 60)
-      return breakLength > 0 ? setBreakLength(breakLength + 1) : null;
+    if (breakLength < 60) setBreakLength(breakLength + 1);
   };
 
   const handleBreakDecrease = () => {
-    if (breakLength < 60)
-      return breakLength > 0 ? setBreakLength(breakLength - 1) : null;
+    if (breakLength > 1) setBreakLength(breakLength - 1);
   };
 
   const handleSessionIncrease = () => {
-    if (breakLength < 60)
-      return breakLength > 0 ? setSessionLength(sessionLength + 1) : null;
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+      setTimeLeft(timeLeft + 60);
+    }
   };
 
   const handleSessionDecrease = () => {
-    if (breakLength < 60)
-      return breakLength > 0 ? setSessionLength(sessionLength - 1) : null;
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+      setTimeLeft(timeLeft - 60);
+    }
   };
 
   return (
     <div className="App">
       <div id="wrapper">
-        <h2>25 + 5 Clock</h2>
+        <h2>Pomodoro Clock</h2>
         <div className="session-break-label">
-          <div>
+          <div className="break-section">
             <h3 id="break-label">Break Length</h3>
             <div className="break">
-              <button id="break-increment" onClick={handleBreakIncrease}>
+              <button
+                disabled={play}
+                id="break-increment"
+                onClick={handleBreakIncrease}
+              >
                 +
               </button>
               <strong id="break-length">{breakLength}</strong>
-              <button id="break-decrement" onClick={handleBreakDecrease}>
+              <button
+                disabled={play}
+                id="break-decrement"
+                onClick={handleBreakDecrease}
+              >
                 -
               </button>
             </div>
           </div>
 
-          <div>
+          <div className="session-section">
             <h3 id="session-label">Session Length</h3>
             <div className="session">
-              <button id="session-increment" onClick={handleSessionIncrease}>
+              <button
+                disabled={play}
+                id="session-increment"
+                onClick={handleSessionIncrease}
+              >
                 +
               </button>
               <strong id="session-length">{sessionLength}</strong>
-              <button id="session-decrement" onClick={handleSessionDecrease}>
+              <button
+                disabled={play}
+                id="session-decrement"
+                onClick={handleSessionDecrease}
+              >
                 -
               </button>
             </div>
@@ -59,14 +138,18 @@ function App() {
         <div className="timer-wrapper">
           <div className="timer">
             <div id="timer-label" className="session">
-              Session
+              {title}
             </div>
             <div id="time-left" className="time">
-              {sessionLength}
+              {timeFormater()}
             </div>
           </div>
-          <button id="start_stop">Start/Stop</button>
-          <button id="reset">Reset</button>
+          <button id="start_stop" onClick={handlePlay}>
+            Start/Stop
+          </button>
+          <button id="reset" onClick={handleReset}>
+            Reset
+          </button>
         </div>
       </div>
       <audio
